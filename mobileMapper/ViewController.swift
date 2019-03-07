ffc346fee800a7119c21dc5910935efef4095728
Ian Lee <ianlee@jh-mba158-15.local>
2019-03-07 14:45:32 -0600

    Added Location Manager

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -12,10 +12,12 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
     }
 
 

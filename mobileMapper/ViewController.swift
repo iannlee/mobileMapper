f870058d5bf1681dea8a6025459986959efab567
Ian Lee <ianlee@jh-mba158-15.local>
2019-03-08 15:04:41 -0600

    Added button functionalities

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -27,10 +27,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentLocation = locations[0]
     }
+    
     @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
     }
     
     @IBAction func whenSearchButtonpressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            <#code#>
+        }
     }
 }
 

a34a9382ed3b5b5a2659984cceb27f1a15fd4cd4
Ian Lee <ianlee@jh-mba158-15.local>
2019-03-13 14:48:14 -0500

    added phone number ability

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -36,7 +36,35 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
         let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
         pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
         return pin
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        var currentMapItem = MKMapItem()
+        if let title = view.annotation?.title, let parkName = title {
+            for mapItem in parks {
+                if mapItem.name == parkName {
+                    currentMapItem = mapItem
+                }
+            }
+        }
+        
+        let placeMark = currentMapItem.placemark
+        print(placeMark)
+    
+        func createAlert(_ phoneNumber: String) {
+            let alert = UIAlertController(title: "Phone Number", message: phoneNumber, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
+        
+        if let phoneNumber = currentMapItem.phoneNumber {
+            createAlert(phoneNumber)
+            print(phoneNumber)
+        }
     }
     
     @IBAction func whenZoomButtonPressed(_ sender: Any) {
